561c0d7ae8d1176d99ef0a1cf7b3664ad679eb71
daproclaima <sebastien@devedanos.com>
2022-11-13 02:30:00 +0100

    feat: add current user middleware and handle jwt session

---
+++ middleware/current-user.js
@@ -0,0 +1,23 @@
+import { MODEL_NAME_USER } from '../constants/db/MODEL_NAMES'
+import jwt, { verify } from 'jsonwebtoken'
+import { promisify } from 'util'
+import UnauthorizedError from '../errors/unauthorized'
+
+export default async (context, next) => {
+  try {
+    const authorizationHeader = context.header.authorization || ''
+
+    const token = authorizationHeader.replace('Bearer ', '')
+
+    const result = await verify(token, process.env.JWT_SECRET)
+
+    context.currentUser = await context.app.db[MODEL_NAME_USER].findOrFail({
+      where: {
+        id: result.sub,
+      },
+    })
+    return await next(context)
+  } catch (error) {
+    throw new UnauthorizedError()
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ import Router from "koa-router";
 import bcrypt from "bcrypt";
 import { promisify } from "util";
 import { MODEL_NAME_USER } from "../constants/db/MODEL_NAMES";
+import currentUser from '../middleware/current-user'
 
 const hash = promisify(bcrypt.hash);
 
@@ -16,5 +17,9 @@ router.post("/", async (context) => {
 
   context.body = context.app.serialize(MODEL_NAME_USER, user);
 });
+
+router.get('/me', currentUser, async context => {
+  context.body = context.app.serialize(MODEL_NAME_USER, context.currentUser)
+})
 
 export default router.routes();
